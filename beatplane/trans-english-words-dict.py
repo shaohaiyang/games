965e5c8397e1081070c31c59dcc0e8f6a3ef4ff6
root <root@OPS-HGH-FDI-020.(none)>
2019-11-19 22:36:36 +0800

    update scripts

--- beatplane/trans-english-words-dict.py
+++ beatplane/trans-english-words-dict.py
@@ -1,6 +1,7 @@
 #!/opt/python374/bin/python3.7
 dic = dict()
-file = "little-school-english-words.list"
+#file = "little-school-english-words.list"
+file = "python.txt"
 with open(file,'r') as f:
   for line in f:
     if not line:
